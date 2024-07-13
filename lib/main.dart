3b1c3053237d68fce649fd3695a931849bf1982d
ekankr2 <ekankr2@naver.com>
2024-07-13 16:54:00 +0900

    setstate

--- lib/main.dart
+++ lib/main.dart
@@ -13,19 +13,30 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var a = 1;
+  var name = ['김영숙', '홍길동', '피자집'];
+  var like = [0, 0, 0];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: Text(a.toString()),
-        onPressed: () {
-          setState(() {
-            a++;
-          });
-        },
-      ),
+      appBar: AppBar(),
+      body: ListView.builder(
+          itemCount: 3,
+          itemBuilder: (context, i) {
+            return ListTile(
+              leading: Text(like[i].toString()),
+              title: Image.asset('dog.jpg'),
+              trailing: TextButton(
+                child: Text('like'),
+                onPressed: () {
+                  setState(() {
+                    like[i]++;
+                  });
+                },
+              ),
+            );
+          }),
     ));
   }
 }
