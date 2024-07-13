e34ac7f5c1d7fe7cb3228e07ebc6646bdde51f69
ekankr2 <ekankr2@naver.com>
2024-07-13 10:43:00 +0900

    typography

--- lib/main.dart
+++ lib/main.dart
@@ -11,19 +11,39 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: Text(
-          '제목',
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Colors.blue,
+            body: SizedBox(
+      child: Row(
+        children: [
+          SizedBox(
+            child: Image.asset(
+              'assets/dog.jpg',
+              fit: BoxFit.fill,
+            ),
+            width: 250,
+            height: 250,
+          ),
+          Column(
+            children: [
+              Text('웨르시 코르기'),
+              Text('끌올 10분 전', style: TextStyle(color: Colors.grey),),
+              Text('제목'),
+              SizedBox(
+                child: Row(
+                  children: [
+                    Text('홍길동'),
+                    Row(children: [
+                      Icon(Icons.heart_broken),
+                      Text('4')
+                    ],)
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
       ),
-      body: Container(
-        width: 50,
-        height: 50,
-        decoration: BoxDecoration(
-            border: Border.all(color: Colors.black), color: Colors.pink),
-      ),
-    ));
+    )));
   }
 }
