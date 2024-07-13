3d57a89438315980067da92515e2878f2e8310dd
ekankr2 <ekankr2@naver.com>
2024-07-13 16:39:27 +0900

    stateful

--- lib/main.dart
+++ lib/main.dart
@@ -4,46 +4,28 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var a = 1;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            body: SizedBox(
-      child: Row(
-        children: [
-          SizedBox(
-            child: Image.asset(
-              'assets/dog.jpg',
-              fit: BoxFit.fill,
-            ),
-            width: 250,
-            height: 250,
-          ),
-          Column(
-            children: [
-              Text('웨르시 코르기'),
-              Text('끌올 10분 전', style: TextStyle(color: Colors.grey),),
-              Text('제목'),
-              SizedBox(
-                child: Row(
-                  children: [
-                    Text('홍길동'),
-                    Row(children: [
-                      Icon(Icons.heart_broken),
-                      Text('4')
-                    ],)
-                  ],
-                ),
-              )
-            ],
-          )
-        ],
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
+      floatingActionButton: FloatingActionButton(
+        child: Text(a.toString()),
+        onPressed: () {
+          setState(() {
+            a++;
+          });
+        },
       ),
-    )));
+    ));
   }
 }
