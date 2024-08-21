8aaac663a31b2ae58cd275e6a453c71ef03eaf3a
IkHwan Im <ekankr2@naver.com>
2024-08-21 17:47:43 +0900

    더보기 기능

--- instagram/lib/main.dart
+++ instagram/lib/main.dart
@@ -1,9 +1,10 @@
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'style.dart' as style;
 import 'package:http/http.dart' as http;
-import 'dart:convert';
+
+import 'style.dart' as style;
 
 void main() {
   runApp(MaterialApp(theme: style.theme, home: MyApp()));
@@ -20,13 +21,21 @@ class _MyAppState extends State<MyApp> {
   var tab = 0;
   var data = [];
 
-  getData() async {
-    var result = await http.get(Uri.parse('https://codingapple1.github.io/app/data.json'));
-    var result2 = jsonDecode(result.body);
+  addData(newData) {
     setState(() {
-      data = result2;
-      print(data);
+      data.add(newData);
     });
+  }
+
+  getData() async {
+    var result = await http
+        .get(Uri.parse('https://codingapple1.github.io/app/data.json'));
+    if (result.statusCode == 200) {
+      var result2 = jsonDecode(result.body);
+      setState(() {
+        data = result2;
+      });
+    }
   }
 
   @override
@@ -49,7 +58,13 @@ class _MyAppState extends State<MyApp> {
           )
         ],
       ),
-      body: [Home(data: data,), Text('샵페이지')][tab],
+      body: [
+        Home(
+          data: data,
+          addData: addData,
+        ),
+        Text('샵페이지')
+      ][tab],
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false,
         showUnselectedLabels: false,
@@ -68,33 +83,67 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-class Home extends StatelessWidget {
-  const Home({Key? key, this.data}) : super(key: key);
+class Home extends StatefulWidget {
+  const Home({Key? key, this.data, this.addData}) : super(key: key);
   final data;
+  final addData;
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  var scroll = ScrollController();
+
+  getMore() async {
+    var result = await http
+        .get(Uri.parse('https://codingapple1.github.io/app/more1.json'));
+
+    if (result.statusCode == 200) {
+      var resultJson = jsonDecode(result.body);
+      print(resultJson);
+      widget.addData(resultJson);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    scroll.addListener(() async {
+      if (scroll.position.pixels == scroll.position.maxScrollExtent) {
+        getMore();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: data.length,
-        itemBuilder: (c, i) {
-          return Column(
-            children: [
-              Image.network(data[i]['image']),
-              Container(
-                constraints: BoxConstraints(maxWidth: 600),
-                padding: EdgeInsets.all(20),
-                width: double.infinity,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('좋아요 ${data[i]['likes']}'),
-                    Text(data[i]['user']),
-                    Text(data[i]['content']),
-                  ],
-                ),
-              )
-            ],
-          );
-        });
+    if (widget.data.isNotEmpty) {
+      return ListView.builder(
+          itemCount: widget.data.length,
+          controller: scroll,
+          itemBuilder: (c, i) {
+            return Column(
+              children: [
+                Image.network(widget.data[i]['image']),
+                Container(
+                  constraints: BoxConstraints(maxWidth: 600),
+                  padding: EdgeInsets.all(20),
+                  width: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('좋아요 ${widget.data[i]['likes']}'),
+                      Text(widget.data[i]['user']),
+                      Text(widget.data[i]['content']),
+                    ],
+                  ),
+                )
+              ],
+            );
+          });
+    }
+
+    return Text('로딩중임');
   }
 }
